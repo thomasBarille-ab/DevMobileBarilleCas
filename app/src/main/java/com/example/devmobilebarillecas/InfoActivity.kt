94f7049ee71c2b1e3c48c72b525d5a2b948b7ccc
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-04-04 15:23:38 +0200

    save, added student activity for thomas, need to do it for me.

--- app/src/main/java/com/example/devmobilebarillecas/InfoActivity.kt
+++ app/src/main/java/com/example/devmobilebarillecas/InfoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.devmobilebarillecas
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -10,14 +11,15 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
-        val button1 = findViewById<Button>(R.id.buttonBarilleThomas)
-        val button2 = findViewById<Button>(R.id.buttonCasMarcOlivier)
+        val buttonBarilleThomas = findViewById<Button>(R.id.buttonBarilleThomas)
+        val buttonCasMarcOlivier = findViewById<Button>(R.id.buttonCasMarcOlivier)
 
-        button1.setOnClickListener {
-            // Action à effectuer lorsque le bouton 1 est cliqué
+        buttonBarilleThomas.setOnClickListener {
+            val intent = Intent(this, StudentActivity::class.java)
+            startActivity(intent)
         }
 
-        button2.setOnClickListener {
+        buttonCasMarcOlivier.setOnClickListener {
             // Action à effectuer lorsque le bouton 2 est cliqué
         }
     }
