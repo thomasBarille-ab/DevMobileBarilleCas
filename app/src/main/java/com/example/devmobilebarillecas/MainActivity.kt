fe1411b74f1137520895dd9573d4c600d26a3253
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-04-04 14:18:58 +0200

    added splash image, buttons on mainActivity

--- app/src/main/java/com/example/devmobilebarillecas/MainActivity.kt
+++ app/src/main/java/com/example/devmobilebarillecas/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.devmobilebarillecas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var splashImage: ImageView
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        splashImage = findViewById(R.id.splash_image)
+        button1 = findViewById(R.id.buttonInfo)
+        button2 = findViewById(R.id.buttonProducts)
+
+        // Cacher les boutons
+        button1.visibility = View.GONE
+        button2.visibility = View.GONE
+
+        // Faire disparaître l'image de splash après 2 secondes
+        Handler().postDelayed({
+            splashImage.animate().alpha(0f).setDuration(1000).withEndAction {
+                splashImage.visibility = View.GONE
+                // Afficher les boutons après la disparition de l'image de splash
+                button1.visibility = View.VISIBLE
+                button2.visibility = View.VISIBLE
+            }
+        }, 5000)
+
+        // Ajouter les listeners pour les clics sur les boutons
+        button1.setOnClickListener {
+            Toast.makeText(this, "Clic sur le bouton 1", Toast.LENGTH_SHORT).show()
+        }
+        button2.setOnClickListener {
+            Toast.makeText(this, "Clic sur le bouton 2", Toast.LENGTH_SHORT).show()
+        }
     }
 }
