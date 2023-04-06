3a57e24af0bd81aca2e1250e8e2a9eb941467782
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-04-06 15:13:23 +0200

    Added header on each screen needed.

--- app/src/main/java/com/example/devmobilebarillecas/StudentActivity.kt
+++ app/src/main/java/com/example/devmobilebarillecas/StudentActivity.kt
@@ -1,6 +1,7 @@
 package com.example.devmobilebarillecas
 
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,11 @@ class StudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
+
+        val backButton = findViewById<ImageButton>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish()
+        }
 
         val studentInfos = intent.getStringExtra("student_infos")
         val studentName = intent.getStringExtra("student_name")
